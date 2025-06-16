97f4be0c83980fb7c060ef59b217d6db1daa31ce
alex <apple@apples-MacBook-Pro.local>
2025-06-16 14:23:53 +0800

    vsc setup

--- set_1.py
+++ set_1.py
@@ -1,3 +1,10 @@
+#先苦後甜
+import sys
+from os import path
+if(path.exists('input.txt')):
+    sys.stdin = open("input.txt","r")
+    sys.stdout = open("output.txt","w")
+    
 def savings(gross_pay, tax_rate, expenses):
     save = int((1-tax_rate)*gross_pay)
     save-=expenses
@@ -10,3 +17,5 @@ def material_waste(total_material, material_units, num_jobs, job_consumption):
 def interest(principal, rate, periods):
     final = principal+principal*(rate*periods)
     return int(final)
+
+print(1)
